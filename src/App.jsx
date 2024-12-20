cdde108ec51ce20c007f5f339a1bde187d76a838
Nousad <nousadaliinps@gmail.com>
2024-12-20 08:57:46 +0530

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      APP
+        <h1 className="text-3xl font-bold underline">
+           Hello world!
+        </h1>
     </div>
   )
 }
