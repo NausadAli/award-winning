3af335026ef868d21b3aa8e216500e6b75a93e22
Nousad <nousadaliinps@gmail.com>
2024-12-20 12:06:48 +0530

    gsap library aded

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <main>
-        <h1 className="text-5xl text-violet-300 font-bold underline">
-           Hello world!
-        </h1>
+    <main className='relative min-h-screen w-screen overflow-hidden'>
+          <Hero/>
     </main>
   )
 }
