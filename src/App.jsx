c15ce7ee6239a5af48ec3417b22aef08a611ead5
Nousad <nousadaliinps@gmail.com>
2025-01-14 20:18:59 +0530

    Some Changes Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@
 import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
+import Feature from './components/Feature'
+import Story from './components/Story'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
@@ -9,12 +13,10 @@ const App = () => {
       <Navbar />
       <Hero/>
       <About />
-
-
-
-
-
-
+      <Feature/>
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   )
 }
